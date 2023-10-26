762ac1502cb2665766f26355f95433c1bdf89827
Cocera <alcocera@gmail.com>
2023-10-26 13:58:41 +0200

    ex2 finished

--- exercise2/index.js
+++ exercise2/index.js
@@ -1,6 +1,42 @@
 const express = require("express");
 const app = express();
-const PORT = 8080
+const PORT = 8080;
+
+app.get("/", (req, res) => {
+    res.status(200).send("Bienvenido!");
+});
+
+app.get("/productos", (req, res) => {
+    res.status(200).send("Listado de productos");
+});
+
+app.post("/productos", (req, res) => {
+    res.status(201).send("Crear un producto");
+});
+
+app.put("/productos", (req, res) => {
+    res.status(200).send("Actualizar un producto");
+});
+
+app.delete("/productos", (req, res) => {
+    res.status(200).send("Borrar un producto");
+});
+
+app.get("/usuarios", (req, res) => {
+    res.status(200).send("Listado de usuarios");
+});
+
+app.post("/usuarios", (req, res) => {
+    res.status(201).send("Crear usuario");
+});
+
+app.put("/usuarios", (req, res) => {
+    res.status(200).send("Actualizar un usuario");
+});
+
+app.delete("/usuarios", (req, res) => {
+    res.status(200).send("Borrar usuario");
+});
 
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
